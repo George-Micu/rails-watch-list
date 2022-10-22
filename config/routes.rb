444d39a8f4b072b89ff4afef1b896ebcd99000e7
Alex Micu <alex.micu2012@yahoo.com>
2022-10-22 17:02:30 -0400

    almost finished the views

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "list#index"
+  resources :list, only: %i[index show new create] do
+    resources :bookmark, only: %i[new create]
+  end
+  resources :bookmark, only: [:destroy]
 end
